774adb567295ea61d6e35ca32fb6590d423da577
nico-ruiz <nicoruiz987@gmail.com>
2025-07-16 11:46:16 -0300

    Funciones para llevar los datos al backend en login y registro incorporadas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,42 @@ import { Link } from 'react-router-dom'
 import "../css/auth.css"
 
 export default function Login() {
-  return (
+    async function login() {
+        const email = document.querySelector('#email').value.trim()
+        const password = document.querySelector('#password').value.trim()
+
+        const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        let camposValidos = 0;
+
+        if(!email_regex.test(email)){
+            document.querySelector('#email_login').style.display = "inline"
+            camposValidos++;
+        }
+
+        if(camposValidos === 0) {
+            const datos = {email: email, password: password}
+
+            try {
+                const res = await fetch(import.meta.env.VITE_LOGIN_URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(datos)
+                })
+                const resultado = await res.json()
+                console.log(resultado)
+
+            } catch (error) {
+                console.log("Error al logear al usuario: ", error)
+
+            }
+
+        }
+    }
+  
+    return (
     
     <div id="auth-container" className="auth-container">
         <div id="login-page" className="auth-form">
@@ -11,16 +46,18 @@ export default function Login() {
             <p className="subtitulo">Sistema Leitner para memorización efectiva</p>
             
             <form id="login-form" className="space-y-4">
-                <div>
-                    <label for="email" className="label">Correo electrónico</label>
+                <div className='field-container'>
+                    <label htmlFor="email" className="label">Correo electrónico</label>
                     <input type="email" id="email" className="input" required="" />
+                    <p id='email_login' className='p_campo_invalido'>Campo inválido</p>
+                </div>
+                <div className='field-container'>
+                    <label htmlFor="password" className="label">Contraseña</label>
+                    <input type="password" id="password" className="input" required="" />
+                    <p id='pass_login' className='p_campo_invalido'>Campo inválido</p>
                 </div>
                 <div>
-                    <label for="password" className="label">Contraseña</label>
-                    <input type="text" id="password" className="input" required="" />
-                </div>
-                <div>
-                    <button type="submit" className="btn_submit">Iniciar sesión</button>
+                    <button type="button" className="btn_submit" onClick={login}>Iniciar sesión</button>
                 </div>
             </form>
             
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -3,26 +3,75 @@ import { Link } from 'react-router-dom'
 import "../css/auth.css"
 
 export default function Registro() {
+
+    async function registrarUsuario(){
+        const nombre = document.querySelector("#reg-name").value.trim()
+        const email = document.querySelector("#reg-email").value.trim()
+        const password = document.querySelector("#reg-password").value.trim()
+
+        const nombre_regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ0-9._-]{4,20}$/
+        const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        let camposValidos = 0;
+
+        if(!nombre_regex.test(nombre)){
+            document.querySelector('#nombre_reg').style.display = "inline"
+            camposValidos++;
+        }
+        
+        if(!email_regex.test(email)){
+            document.querySelector('#email_reg').style.display = "inline"
+            camposValidos++;
+        }
+
+        if(camposValidos === 0){
+            const datos = {
+                nombre: nombre,
+                email: email,
+                password: password
+            }
+
+            try{
+                const res = await fetch(import.meta.env.VITE_REGISTRO_URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(datos)
+                })
+                const resultado = await res.json();
+                console.log(resultado)
+
+            } catch(error) {
+                console.error("Error al registrar al usuario: ", error)
+
+            }
+        }
+    }
+
   return (
     <div className="auth-container">
       <div id="register-page" className="auth-form">
           <h2 className="titulo">Crear cuenta</h2>
 
           <form id="register-form" className="space-y-4">
-              <div>
+              <div className='field-container'>
                   <label for="reg-name" className="label">Nombre</label>
                   <input type="text" id="reg-name" className="input" required="" />
+                  <p id='nombre_reg' className='p_campo_invalido'>Campo inválido</p>
               </div>
-              <div>
+              <div className='field-container'>
                   <label for="reg-email" className="label">Correo electrónico</label>
                   <input type="email" id="reg-email" className="input" required="" />
+                  <p id='email_reg' className='p_campo_invalido'>Campo inválido</p>
               </div>
-              <div>
+              <div className='field-container'>
                   <label for="reg-password" className="label">Contraseña</label>
-                  <input type="text" id="reg-password" className="input" required="" />
+                  <input type="password" id="reg-password" className="input" required="" />
+                  <p id='pass_reg' className='p_campo_invalido'>Campo inválido</p>
               </div>
               <div>
-                  <button type="submit" className="btn_submit">Registrarse</button>
+                  <button type="button" className="btn_submit" onClick={registrarUsuario}>Registrarse</button>
               </div>
           </form>
 
